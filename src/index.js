6af9850cbd5c2772289a68aeb595b6ea2d6563f1
Akinwale Habib <akinwalehabib@hotmail.com>
2021-10-08 03:36:42 +0100

    ch: add redux store application

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+import middlewares from './middlewares';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(reducers, middlewares);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/reducers/jobs.js
@@ -0,0 +1,8 @@
+const jobs = (state = [], action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default jobs;
